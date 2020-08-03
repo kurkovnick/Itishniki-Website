29f7c2f6bf6885636ca58a67bf991ccecab9bac9
Nick Kurkov <nickkurkov@Nicks-MacBook-Air.local>
2020-08-03 16:11:04 -0700

    Initial commit

---
+++ Array-dot-diff.js
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/array-dot-diff
+
+// Array-dot-diff
+
+function arrayDiff(a, b) {
+    return (a.filter()) - (b.filter());
+}
---
+++ filterEvenLengthWords.js
@@ -0,0 +1,8 @@
+//https://www.codewars.com/kata/filterevenlengthwords
+
+//filterEvenLengthWords
+
+function filterEvenLengthWords(words) {
+    return words.filter(words => words.length % 2 === 0)
+}
+
